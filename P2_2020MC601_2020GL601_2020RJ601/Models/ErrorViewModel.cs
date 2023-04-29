3084e7f9cbbbe7dc28d6374d2e62371e1221b1da
joselias0 <jose.morales3@catolica.edu.sv>
2023-04-29 08:13:00 -0600

    Add project files.

---
+++ P2_2020MC601_2020GL601_2020RJ601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020MC601_2020GL601_2020RJ601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
