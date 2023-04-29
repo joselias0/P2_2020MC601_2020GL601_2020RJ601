3167ab331707c4734552c9c62b3f2f3fdd498fcb
joselias0 <jose.morales3@catolica.edu.sv>
2023-04-29 09:01:22 -0600

    inyeccion de base de datos y creacion del contexto

---
+++ P2_2020MC601_2020GL601_2020RJ601/Models/casos_covidContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P2_2020MC601_2020GL601_2020RJ601.Models
+{
+    public class casos_covidContext : DbContext
+    {
+        public casos_covidContext(DbContextOptions<casos_covidContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CasosReportados> CasosReportados { get; set; }
+        public DbSet<Departamentos> Departamentos { get; set; }
+        public DbSet<Generos> Generos { get; set; }
+    }
+}
--- P2_2020MC601_2020GL601_2020RJ601/Program.cs
+++ P2_2020MC601_2020GL601_2020RJ601/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using P2_2020MC601_2020GL601_2020RJ601.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<casos_covidContext>(options => options.UseSqlServer(
+                  builder.Configuration.GetConnectionString("casos_covidDbConnection")
+               ));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
