80d9bd716205506bc05683a0569790ff5f667f6c
mauriciorodriguez4 <mauricio.rodriguez4@catolica.edu.sv>
2023-04-29 09:34:21 -0600

    Creacion del formulario registro Covid

---
+++ P2_2020MC601_2020GL601_2020RJ601/Controllers/CasosReportadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2_2020MC601_2020GL601_2020RJ601.Controllers
+{
+	public class CasosReportadosController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
