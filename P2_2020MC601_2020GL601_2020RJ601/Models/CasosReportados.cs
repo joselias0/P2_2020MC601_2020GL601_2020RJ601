3f5011859b5bf4cdd198a04462123cdcbb351df7
GabrielFernandoGarciaLemus <gabriel.garcia@catolica.edu.sv>
2023-04-29 10:51:35 -0600

    correccion de errores

--- P2_2020MC601_2020GL601_2020RJ601/Models/CasosReportados.cs
+++ P2_2020MC601_2020GL601_2020RJ601/Models/CasosReportados.cs
@@ -8,8 +8,8 @@ namespace P2_2020MC601_2020GL601_2020RJ601.Models
         public int id_caso {get; set;}
         public int? id_departamento { get; set;}
         public int? id_genero { get; set; }
-        public string? casosConfirmados { get; set; }
-        public string? casosRecuperados { get; set; }
-        public string? casosFallecidos { get; set; }
+        public int? casosConfirmados { get; set; }
+        public int? casosRecuperados { get; set; }
+        public int? casosFallecidos { get; set; }
     }
 }
