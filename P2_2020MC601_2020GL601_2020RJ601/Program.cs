3996aab46cf2642a1910f8dc2020dd9b38664cf6
GabrielFernandoGarciaLemus <gabriel.garcia@catolica.edu.sv>
2023-04-29 11:04:12 -0600

    cambios en layout

--- P2_2020MC601_2020GL601_2020RJ601/Program.cs
+++ P2_2020MC601_2020GL601_2020RJ601/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=CasosReportados}/{action=Index}/{id?}");
 
 app.Run();
