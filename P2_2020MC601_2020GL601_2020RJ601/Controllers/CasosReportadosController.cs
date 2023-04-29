99266affdfc7700e81ffb00a43f812455eb11536
joselias0 <jose.morales3@catolica.edu.sv>
2023-04-29 10:07:10 -0600

    creacion de listas y actualizacion de Index

--- P2_2020MC601_2020GL601_2020RJ601/Controllers/CasosReportadosController.cs
+++ P2_2020MC601_2020GL601_2020RJ601/Controllers/CasosReportadosController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2020MC601_2020GL601_2020RJ601.Models;
 
 namespace P2_2020MC601_2020GL601_2020RJ601.Controllers
 {
 	public class CasosReportadosController : Controller
 	{
-		public IActionResult Index()
+        private readonly casos_covidContext _CovidContext;
+
+        public CasosReportadosController(casos_covidContext covidContext)
+        {
+            _CovidContext = covidContext;
+        }
+        public IActionResult Index()
 		{
-			return View();
+            var listadoDeDepartamento = (from d in _CovidContext.Departamentos select d).ToList();
+            ViewData["listadoDeDepartamento"] = new SelectList(listadoDeDepartamento, "id_departamento", "nombre_departamento");
+
+            var listadoDeGenero = (from g in _CovidContext.Generos select g).ToList();
+            ViewData["listadoDeGenero"] = new SelectList(listadoDeGenero, "id_genero", "nombre_genero");
+
+            var listadoDeCasos = (from cr in _CovidContext.CasosReportados join g in _CovidContext.Generos on cr.id_genero equals g.id_genero
+                                  join d in _CovidContext.Departamentos on cr.id_departamento equals d.id_departamento
+                                  select new
+                                  {
+                                      departamento = d.nombre_departamento,
+                                      genero=g.nombre_genero,
+                                      confirmados=cr.casosConfirmados,
+                                      recuperados=cr.casosRecuperados,
+                                      fallecidos=cr.casosFallecidos
+                                  }).ToList();
+
+            ViewData["listadoDeCasos"] = listadoDeCasos;
+            return View();
 		}
 	}
 }
